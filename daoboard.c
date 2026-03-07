542393a9153f24ebce7aba1ea59e9c3bcbcf24aa
Zyhru <zyhrups3@gmail.com>
2026-03-07 05:14:50 -0500

    Switches are being detected

--- daoboard.c
+++ daoboard.c
@@ -1,11 +1,14 @@
 /* avr */
 #include <avr/io.h>
+#include <avr/cpufunc.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include <avr/pgmspace.h>
+#include <stdio.h>
 #include <util/delay.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "LUFA/Common/Common.h"
 #include "descriptors.h"
@@ -17,6 +20,9 @@ void EVENT_USB_Device_Connect(void);
 void EVENT_USB_Device_Disconnect(void);
 void EVENT_USB_Device_ConfigurationChanged(void);
 void EVENT_USB_Device_ControlRequest(void);
+
+uint8_t pinf_state;
+
 
 USB_ClassInfo_CDC_Device_t usb_cdc_interface =
 	{ .Config =
@@ -44,11 +50,24 @@ USB_ClassInfo_CDC_Device_t usb_cdc_interface =
 	};
 
 typedef enum {
-    SWITCH_1,
-    SWITCH_2,
-    SWITCH_3,
-    SWITCH_4,
-} keyswitch_t;
+    DEFAULT,  // 0
+    SWITCH_1, // 1
+    SWITCH_2, // 2
+    SWITCH_3, // 3
+    SWITCH_4, // 4
+} KeySwitch;
+
+typedef struct {
+    char* str;
+    KeySwitch sw;
+} KeyLayout;
+
+#define ROWS 2
+#define COLS 2
+KeyLayout kl[ROWS][COLS] = {
+    { {}, {}              },
+    { {}, {}              },
+};
 
 /* setting col and row to 1 */
 #define COL0 (1 << PORTF0) 
@@ -59,8 +78,6 @@ typedef enum {
 
 #define DELAY_MS 10
 #define MAX_SWITCHES 4
-
-uint8_t matrix_scan();
 
 /*
  * COL are input
@@ -76,23 +93,69 @@ uint8_t matrix_scan();
  *
  * DDRx 1 = Output
  * DDRx 0 = Input
+ *
+ *
  */
 
 static FILE USBSerialStream;
 
+/*
+ * NOTE: Reading the Pin Value:
+ * Port pin can be read throug the PINF Register Bit
+ * Example:
+ * PINXN := x = letter of the register, n = number of pin
+ * PINF0
+ * PINF1
+ * PINF2
+ *
+ * When reading back a software assigned pin value 
+ * you must use a nop instruction.
+ *
+ *
+ */
+static void send_buffer(uint8_t state, uint8_t reg, double ms)  {
+    char buffer[50];
+    char str[10];
+    
+    if(reg == PORTF) strcpy(str, "PORTF");
+    if(reg == PINF) strcpy(str, "PINF");
+    if(reg == MCUCR) strcpy(str, "MCUCR");
+    state = reg;
+        
+     _NOP(); // Synchronization
+    sprintf(buffer, "TEST: %s (2/27/26): 0x%02X\r\n", str, state);
+    CDC_Device_SendString(&usb_cdc_interface, buffer);
+    _delay_ms(ms);
+}
+
+static void send_pinf_buffer(char *msg, double ms) {
+    char buffer[50];
+    _NOP();
+    snprintf(buffer, sizeof(buffer), "%s\r\n", msg);
+    CDC_Device_SendString(&usb_cdc_interface, buffer);
+    _delay_ms(ms);
+}
+
+/*
+ *
+PORTF (2/27/26): 0x30
+MCUCR (2/27/26): 0x00
+PINF (2/27/26): 0x02
+ *
+ */
 void daoboard_init(void) {
-    // Set columns as input
-    DDRF &= ~(COL0 | COL1);
+    // input + pull-ups
+    PORTF |= (1 << PF1 | 1 << PF0);
+    DDRF &= ~((1 << PF1) | (1 << PF0)); 
 
-    // Enable the internal pull-up resistors
-    PORTF |= (COL0 | COL1);
-
-    // Set rows as output
-    DDRF |= (ROW0 | ROW1);
-
-    // Drive ROWs as LOW (initially)
-    PORTF &= ~(ROW0 | ROW1);
-
+    // disabling jtag bits
+    MCUCR |= (1 << JTD);
+    MCUCR |= (1 << JTD);
+   
+    // output high
+    PORTF |= (1 << PF5 | 1 << PF4);
+    DDRF |= (1 << PF5 | 1 << PF4);  
+   
     MCUSR &= ~(1 << WDRF);
     wdt_disable();
 	clock_prescale_set(clock_div_1);
@@ -102,75 +165,125 @@ void daoboard_init(void) {
     GlobalInterruptEnable();
 }
 
-int main(void) {
-    daoboard_init();
-
-    // scanning for a key press
-    for(;;) {
-        
-        CDC_Device_USBTask(&usb_cdc_interface);
-        USB_USBTask();
-        
-        CDC_Device_ReceiveByte(&usb_cdc_interface);
-        _delay_ms(1000);
-        CDC_Device_SendString(&usb_cdc_interface, "ZYHRU\r\n");
-        
-        #if 0
-        uint8_t key_pressed = matrix_scan();
-        switch(key_pressed) {
-            case SWITCH_1:
-                break;
-            case SWITCH_2:
-                break;
-            case SWITCH_3:
-                break;
-            case SWITCH_4:
-                break;
-
-        }
-
-        _delay_ms(DELAY_MS);
-        #endif
-
-    }
-    return 0;
+// set PORTFx to 0 as input
+void disable_pull_ups(uint8_t pin) { 
+    PORTF &= ~(pin);
 }
 
-//TODO: Redo this
-uint8_t matrix_scan(void) {
+// set PORTFx to 1 as input
+void enable_pull_ups(uint8_t pin) { 
+    PORTF |= pin;
+}
+
+void send_string(char *msg) {
+    CDC_Device_SendString(&usb_cdc_interface, msg);
+}
+
+/*
+ * Drive COLUMNS low
+ * PF0
+ * Read PINF and check if PF4 is low: if so sw1 is pressed
+ * Read PINF and check if PF5 is low: is so sw3 is pressed
+ *
+ *
+ * PF1
+ * Read PINF and check if PF4 is low: if so sw2 is pressed
+ * Read PINF and check if PF5 is low: is so sw4 is pressed
+ */
+uint8_t daoboard_scan(void) {
     uint8_t key = 0;
+   
+    // pinf will 0x73 -> 0b1110011
+    // 0b01110011 AND
+    // 0b00010000
+    // ----------
+    // 0b00010000
+    // !0b00010000 = 0
+   
+    uint8_t portf_state;
+    uint8_t pin_state;
     
-    PORTF |= COL0;
-    PORTF &= ~COL1;
-    _delay_ms(DELAY_MS);
+    // ================================== //
+    /* scanning COL0  */
+    PORTF &= ~(1 << PF4); // ROW0
+    _NOP();
 
-    if(PINF & ROW0) {
-        // sw1
-        key = 0;
+    // 0,0
+    if(!(PINF & (1 << PF0))) {
+        key = 1;     
+        send_pinf_buffer("(0,0)", 20);
     }
 
-    if(PINF & ROW1) {
-        // sw2
-        key = 1;
+    // 0,1
+    if(!(PINF & (1 << PF1))) {
+        send_pinf_buffer("(0,1)", 20);
+        key = 3;     
     }
-
-    // disable COL0 now
-    PORTF &= ~COL0;
-    PORTF |= COL1;
-    _delay_ms(DELAY_MS);
     
-    if(PINF & ROW0) {
-        // sw3
-        key = 2;
-    }
+    PORTF |= (1 << PF4);
+    _NOP();
+   // --------------------------------- // 
 
-    if(PINF & ROW0) {
-        // sw4
-        key = 3;
+    /* scanning COL1 */
+    // 1,0
+    PORTF &= ~(1 << PF5); // Drive PF5 LOW
+    _NOP();
+    if(!(PINF & (1 << PF0))) {
+        send_pinf_buffer("(1,0)", 20);
+        key = 2;     
     }
-
-    PORTF &= ~(COL0 | COL1);
+    
+    // 1,1
+    if(!(PINF & (1 << PF1))) {
+        send_pinf_buffer("(1,1)", 20);
+        key = 4;     
+    }
+    PORTF |= (1 << PF5);
+    _NOP();
+    
     return key;
+}
+
+int main(void) {
+    daoboard_init();
+    uint8_t pin_state;
+    uint8_t port_state;
+    uint8_t jtag_state;
+    
+    char pin_buffer[50];
+    char port_buffer[50];
+
+    for(;;) {
+        CDC_Device_USBTask(&usb_cdc_interface);
+        USB_USBTask();
+        CDC_Device_ReceiveByte(&usb_cdc_interface);
+        //send_buffer(pin_state, PINF, 500);
+        //send_buffer(port_state, PORTF, 500);
+        //send_buffer(jtag_state, MCUCR, 500);
+
+        uint8_t key_pressed = daoboard_scan();
+        switch(key_pressed) {
+            case SWITCH_1:
+                CDC_Device_SendString(&usb_cdc_interface, "Pressed Switch 1\r\n");
+                _delay_ms(10);
+                break;
+            case SWITCH_2:
+                CDC_Device_SendString(&usb_cdc_interface, "Pressed Switch 2\r\n");
+                _delay_ms(10);
+                break;
+            case SWITCH_3:
+                CDC_Device_SendString(&usb_cdc_interface, "Pressed Switch 3\r\n");
+                _delay_ms(10);
+                break;
+            case SWITCH_4:
+                CDC_Device_SendString(&usb_cdc_interface, "Pressed Switch 4\r\n");
+                _delay_ms(10);
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
 }
 
 // COL0 has sw1 and sw2
